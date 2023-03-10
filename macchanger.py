d4998b2f10cdd613c2206b6a6bee5671562a9eab
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-03-09 22:05:26 -0600

    First rendition of a MAC address changer.

---
+++ macchanger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Script Name:      MAC Address Changer
+# Author:           Vincent Bailey
+# Last Rev:         03/9/2023
+# Purpose:          This script will chnage the MAC address of a Linux machine.
+
+import subprocess
+
+subprocess.call("ifconfig", shell=True)
