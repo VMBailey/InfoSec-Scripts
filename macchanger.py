884d26d804034869e7eeb04f5f464789970fdfef
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-03-10 15:43:17 -0600

    Added content to parse args variable.

--- macchanger.py
+++ macchanger.py
@@ -2,7 +2,7 @@
 
 # Script Name:      MAC Address Changer
 # Author:           Vincent Bailey
-# Last Rev:         03/9/2023
+# Last Rev:         03/10/2023
 # Purpose:          This script will chnage the MAC address of a Linux machine.
 
 import subprocess
@@ -12,14 +12,17 @@ import optparse
 # options should the user type "python macchanger.py --help" into their terminal.
 parser = optparse.OptionParser()
 
-# This variable lays out every option for our help menu.
+# These variable lay out every option for our help menu.
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+parser.add_option("-m", "--mac", dest="newMac", help="Your MAC address of choice.")
 
 # This variable helps to understand the options added into the "add_option" method.
-parser.parse_args()
+(options, arguments) = parser.parse_args()
 
-interface = input("Which interface would you like to change? ")
-newMac = input("What MAC address would you like to assign to this device? ")
+#interface = input("Which interface would you like to change? ")
+#newMac = input("What MAC address would you like to assign to this device? ")
+interface = options.interface
+newMac = options.newMac
 
 print("[+] Changing the MAC address for " + interface + " to " + newMac)
 
