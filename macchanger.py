f19e9ee5cc0403f3d50367161365e3061cb0c3c9
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-03-11 12:03:07 -0600

    Added if statement to get_arguments function.

--- macchanger.py
+++ macchanger.py
@@ -15,20 +15,20 @@ import optparse
 # Global Variables
 ##############################################################################
 #interface = input("Which interface would you like to change? ")
-#new_Mac = input("What MAC address would you like to assign to this device? ")
+#new_mac = input("What MAC address would you like to assign to this device? ")
 #interface = options.interface
-#new_Mac = options.new_Mac
+#new_mac = options.new_mac
 
 ##############################################################################
 # Change MAC Function
 ##############################################################################
-def change_Mac(interface, new_Mac):
-    print("[+] Changing the MAC address for " + interface + " to " + new_Mac)
+def change_mac(interface, new_mac):
+    print("[+] Changing the MAC address for " + interface + " to " + new_mac)
 
     # By enclosing our linux commands in quotations, this will prevent a user
     # from using any other linux commands while inputting their variables.
     subprocess.call(["ifconfig ", interface, " down"])
-    subprocess.call(["ifconfig ", interface, " hw", "ether", new_Mac])
+    subprocess.call(["ifconfig ", interface, " hw", "ether", new_mac])
     subprocess.call(["ifconfig ", interface, " up"])
 
 ##############################################################################
@@ -41,15 +41,24 @@ def get_arguments():
 
     # These variable lay out every option for our help menu.
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-    parser.add_option("-m", "--mac", dest="new_Mac", help="Your MAC address of choice.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Your MAC address of choice.")
 
     # By changing this variable from (options, arguments) = get_arguments() to 
     # a return statement, this tells python to return any variables that are
     # stored within parser.parse_args() any time this function is called.
-    return parser.parse_args()
+    # return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        # code to handle error
+        parser.error("Please specify an interface. use -- help for more info.")
+    elif not options.new_mac:
+        # code to handle error
+        parser.error("Please specify a valid MAC address. use -- help for more info.")
+    return options
 
 ##############################################################################
 # Main
 ##############################################################################
-(options, arguments) = get_arguments()
-change_Mac(options.interface, options.new_Mac)
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
