4c2fbcf3a863572a2f4ad8a93fa2a4cf03864eeb
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-03-15 21:03:20 -0500

    Edited comments for parser variable.

--- macchanger.py
+++ macchanger.py
@@ -35,13 +35,14 @@ def change_mac(interface, new_mac):
 # Get Arguments Function
 ##############################################################################
 def get_arguments():
-    # This variable contains the OpttionPArser class which allows us to add help
-    # options should the user type "python macchanger.py --help" into their terminal.
+    # The variable below contains the OptionParser class which allows us to add
+    # descriptions to our -i and -m commands should the user type
+    # "python3 macchanger.py --help" into their terminal.
     parser = optparse.OptionParser()
 
     # These variable lay out every option for our help menu.
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="Your MAC address of choice.")
+    parser.add_option("-i", "--interface", dest="interface", help="This is the interface that you wish to change.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Your new MAC address.")
 
     # By changing this variable from (options, arguments) = get_arguments() to 
     # a return statement, this tells python to return any variables that are
